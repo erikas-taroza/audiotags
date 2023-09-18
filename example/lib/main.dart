7efc200090733c9ff05ab7f2eaeb683963ae37b7
Erikas Taroza <erikastaroza@gmail.com>
2023-09-17 19:10:16 -0700

    Formatting, use specific version of FRB in Rust project

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,8 +7,8 @@ import 'package:audiotags/audiotags.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 void main() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -18,81 +18,78 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp>
-{
-    String path = "";
+class _MyAppState extends State<MyApp> {
+  String path = "";
 
-    @override
-    void initState()
-    { super.initState(); }
+  @override
+  void initState() {
+    super.initState();
+  }
 
-    @override
-    Widget build(BuildContext context)
-    {
-        return MaterialApp(
-            home: Scaffold(
-                appBar: AppBar(
-                    title: const Text('Audiotags Example'),
-                ),
-                body: Center(
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                            ElevatedButton(
-                                child: const Text("Open"),
-                                onPressed: () async {
-                                    if(Platform.isAndroid || Platform.isIOS) await Permission.storage.request();
-                                    FilePickerResult? r = await FilePicker.platform.pickFiles();
-                                    if(r != null) path = r.files.single.path!;
-                                },
-                            ),
-                            const SizedBox(height: 10),
-                            ElevatedButton(
-                                child: const Text("Write"),
-                                onPressed: () async {
-                                    Tag tag = Tag(
-                                        title: "Title",
-                                        artist: "Artist",
-                                        album: "Album",
-                                        genre: "Genre",
-                                        year: 2000,
-                                        pictures: [
-                                            Picture(
-                                                bytes: Uint8List.fromList([0, 0, 0, 0]),
-                                                mimeType: MimeType.none,
-                                                pictureType: PictureType.other
-                                            )
-                                        ]
-                                    );
-                                    AudioTags.write(path, tag);
-                                },
-                            ),
-                            const SizedBox(height: 10),
-                            ElevatedButton(
-                                child: const Text("Read"),
-                                onPressed: () async {
-                                    Tag? tag = await AudioTags.read(path);
-                                    String? title = tag?.title;
-                                    String? artist = tag?.artist;
-                                    String? album = tag?.album;
-                                    String? genre = tag?.genre;
-                                    int? year = tag?.year;
-                                    int? duration = tag?.duration;
-                                    List<Picture>? pictures = tag?.pictures;
-
-                                    debugPrint("Title: $title");
-                                    debugPrint("Artist: $artist");
-                                    debugPrint("Album: $album");
-                                    debugPrint("Genre: $genre");
-                                    debugPrint("Year: ${year.toString()}");
-                                    debugPrint("Duration: ${duration.toString()}");
-                                    debugPrint("Pictures: $pictures");
-                                },
-                            ),
-                        ],
-                    )
-                ),
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Audiotags Example'),
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: const Text("Open"),
+              onPressed: () async {
+                if (Platform.isAndroid || Platform.isIOS)
+                  await Permission.storage.request();
+                FilePickerResult? r = await FilePicker.platform.pickFiles();
+                if (r != null) path = r.files.single.path!;
+              },
             ),
-        );
-    }
+            const SizedBox(height: 10),
+            ElevatedButton(
+              child: const Text("Write"),
+              onPressed: () async {
+                Tag tag = Tag(
+                    title: "Title",
+                    artist: "Artist",
+                    album: "Album",
+                    genre: "Genre",
+                    year: 2000,
+                    pictures: [
+                      Picture(
+                          bytes: Uint8List.fromList([0, 0, 0, 0]),
+                          mimeType: MimeType.none,
+                          pictureType: PictureType.other)
+                    ]);
+                AudioTags.write(path, tag);
+              },
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              child: const Text("Read"),
+              onPressed: () async {
+                Tag? tag = await AudioTags.read(path);
+                String? title = tag?.title;
+                String? artist = tag?.artist;
+                String? album = tag?.album;
+                String? genre = tag?.genre;
+                int? year = tag?.year;
+                int? duration = tag?.duration;
+                List<Picture>? pictures = tag?.pictures;
+
+                debugPrint("Title: $title");
+                debugPrint("Artist: $artist");
+                debugPrint("Album: $album");
+                debugPrint("Genre: $genre");
+                debugPrint("Year: ${year.toString()}");
+                debugPrint("Duration: ${duration.toString()}");
+                debugPrint("Pictures: $pictures");
+              },
+            ),
+          ],
+        )),
+      ),
+    );
+  }
 }
